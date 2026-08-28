fe1ba94dd62eb342d2f24f1ba6003567af6ce1c6
Abhi <abhijeetsalalkar122@gmail.com>
2026-08-28 09:16:54 +0530

    cal square

---
+++ ArithmeticOperatorEx3.py
@@ -0,0 +1,7 @@
+#Program for calculate square of given number
+#By using predined function sqrt() of math module
+
+import math
+n=float(input("Enter a number for calculate square root:"))
+result=math.sqrt(n)
+print("The square root of({})={}".format(n,result))
