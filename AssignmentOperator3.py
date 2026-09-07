8d24a144c79c0c30f898184cf455d5c015c45464
Abhi <abhijeetsalalkar122@gmail.com>
2026-09-07 19:34:49 +0530

    swapped two number logic 2

--- AssignmentOperator3.py
+++ AssignmentOperator3.py
@@ -1,12 +1,12 @@
-#Program for Swapping of any Two Integer Values--Logic-2
+#Program for Swapping of any Two Integer Values--Logic-3
 a,b=input("Enter Number of a:"),input("Enter Number of b:")
 print("-"*90)
 print("Display Number of a:{}".format(a))
 print("Display Number of b:{}".format(b))
 print("-"*90)
 #Swapping
-t=a
-a=b
-b=t
+a=a+b
+a=b-a
+b=a-b
 print("Swapped Number of a:{}".format(a))
 print("Swapped Number of b:{}".format(b))
