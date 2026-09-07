49385c842c172f86156c344d41dbdc3bc9580a26
Abhi <abhijeetsalalkar122@gmail.com>
2026-09-07 19:19:57 +0530

    swapped two number logic 2

---
+++ AssignmentOperator2.py
@@ -0,0 +1,10 @@
+a,b=input("Enter Number of a:"),input("Enter Number of b:")
+print("-"*90)
+print("Display Number of a:{}".format(a))
+print("Display Number of b:{}".format(b))
+print("-"*90)
+t=a
+a=b
+b=t
+print("Swapped Number of a:{}".format(a))
+print("Swapped Number of b:{}".format(b))
