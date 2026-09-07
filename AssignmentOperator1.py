caae403a96c08386c5d6c64f09e3024c34448c77
Abhi <abhijeetsalalkar122@gmail.com>
2026-09-07 19:34:49 +0530

    swapped two number logic 2

--- AssignmentOperator1.py
+++ AssignmentOperator1.py
@@ -1,8 +1,10 @@
+#Program for Swapping of any Two Integer Values--Logic-1
 a,b=input("Enter Number Of a:"),input("Enter Number Of b:")
 print("-"*90)
 print("Display Number Of a:{}".format(a))
 print("Display Number Of b:{}".format(b))
 print("-"*90)
+#Swapping
 a,b=b,a
 print("Swapped Number Of a:{}".format(a))
 print("Swapped Number Of b:{}".format(b))
