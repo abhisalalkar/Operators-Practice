03e38504076bed7bd97b00f39869be16b9b59755
Abhi <abhijeetsalalkar122@gmail.com>
2026-09-07 19:11:03 +0530

    swapped two number

---
+++ AssignmentOperator1.py
@@ -0,0 +1,8 @@
+a,b=input("Enter Number Of a:"),input("Enter Number Of b:")
+print("-"*90)
+print("Display Number Of a:{}".format(a))
+print("Display Number Of b:{}".format(b))
+print("-"*90)
+a,b=b,a
+print("Swapped Number Of a:{}".format(a))
+print("Swapped Number Of b:{}".format(b))
